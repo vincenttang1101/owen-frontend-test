06bf678579ef1ee874eae0f4b26dfbee26baeb06
vincenttang1101 <tangtrinhquang@gmail.com>
2024-01-19 17:13:19 +0700

    Feat: built header #1

--- src/components/MainLayout/Header/index.tsx
+++ src/components/MainLayout/Header/index.tsx
@@ -1,3 +1,41 @@
+import { Link } from 'react-router-dom'
+import { Logo } from '@assets/icons'
+import './styles.scss'
+import { IoMenu } from 'react-icons/io5'
+
 export default function Header() {
-  return <header>Header</header>
+  return (
+    <header className='header'>
+      {/* Container */}
+      <div className='container'>
+        <div className='body'>
+          {/* Brand */}
+          <div className='brand'>
+            <Link to='/' className='brand__links'>
+              <img src={Logo} alt='Logo' className='brand__logo' />
+              <span className='brand__name'>Todo List</span>
+            </Link>
+          </div>
+
+          {/* navbar */}
+          <nav className='navbar'>
+            <ul className='navbar__list'>
+              <li className='navbar-item'>
+                <Link to='/' className='navbar-item__link'>
+                  Home
+                </Link>
+              </li>
+              <li className='navbar__item'>
+                <Link to='#!' className='navbar-item__link'>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <IoMenu className='menu-icon' />
+        </div>
+      </div>
+    </header>
+  )
 }
--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -3,18 +3,9 @@ import { Header, Footer } from '@components'
 export default function MainLayout({ children }: any) {
   return (
     <>
-      <html lang='en'>
-        <head>
-          <meta charSet='UTF-8' />
-          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-          <title>Document</title>
-        </head>
-        <body>
-          <Header />
-          {children}
-          <Footer />
-        </body>
-      </html>
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </>
   )
 }
