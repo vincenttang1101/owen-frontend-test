111766c75e5fe2decea9e13f9500ae17bbcb8b3c
vincenttang1101 <tangtrinhquang@gmail.com>
2024-01-20 01:05:15 +0700

    Feat: built todo-list #3

--- src/features/TodoList/TaskInputForm/index.tsx
+++ src/features/TodoList/TaskInputForm/index.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
 import { FaPlus, FaPenAlt } from 'react-icons/fa'
 import { nanoid } from 'nanoid'
 import { addTask, toggleAddOrUpdateStatus, updateTask } from '@features/TodoList/todoList.slice'
@@ -6,61 +8,71 @@ import { useAppDispatch, useAppSelector } from '@redux/hook'
 import './styles.scss'
 
 export default function TaskInputForm() {
-  const [title, setTitle] = useState('')
   const { action, valueOfUpdate } = useAppSelector((state) => state.todo.addOrUpdateStatus)
   const dispatch = useAppDispatch()
 
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const title = event.target.value
-    setTitle(title)
-  }
+  const formik = useFormik({
+    initialValues: {
+      title: ''
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().required('Title is required')
+    }),
+    onSubmit: (values) => {
+      if (action === 'update' && valueOfUpdate) {
+        dispatch(
+          updateTask({
+            id: valueOfUpdate.id,
+            title: values.title
+          })
+        )
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+        dispatch(
+          toggleAddOrUpdateStatus({
+            action: 'add'
+          })
+        )
+      } else {
+        dispatch(
+          addTask({
+            id: nanoid(),
+            title: values.title,
+            isCompleted: false
+          })
+        )
+      }
 
-    if (action === 'update' && valueOfUpdate) {
-      dispatch(
-        updateTask({
-          id: valueOfUpdate.id,
-          title
-        })
-      )
-
-      dispatch(
-        toggleAddOrUpdateStatus({
-          action: 'add'
-        })
-      )
-    } else {
-      dispatch(
-        addTask({
-          id: nanoid(),
-          title,
-          isCompleted: false
-        })
-      )
+      formik.resetForm()
     }
-    setTitle('')
-  }
+  })
 
   useEffect(() => {
     if (valueOfUpdate) {
-      setTitle(valueOfUpdate.title)
+      formik.setFieldValue('title', valueOfUpdate.title)
     }
   }, [valueOfUpdate])
 
   return (
-    <form className='task-form' onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder='Enter task'
-        className='task-form__input'
-        value={title}
-        onChange={handleTitleChange}
-      />
-      <button type='submit' className='task-form__btn'>
-        {action === 'add' ? <FaPlus className='task-form__icon' /> : <FaPenAlt className='task-form__icon' />}
-      </button>
-    </form>
+    <>
+      <form className='task-form' onSubmit={formik.handleSubmit}>
+        {/* Task input wrapper */}
+        <div className='task-input-wrapper'>
+          <input
+            type='text'
+            placeholder='Enter task'
+            name='title'
+            className='task-input'
+            value={formik.values.title}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.title && formik.errors.title && <p className='task-input__error'>{formik.errors.title}</p>}
+        </div>
+
+        {/* Task form button */}
+        <button type='submit' className='task-form__btn'>
+          {action === 'add' ? <FaPlus className='task-form__icon' /> : <FaPenAlt className='task-form__icon' />}
+        </button>
+      </form>
+    </>
   )
 }
