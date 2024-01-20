8f490b79b136fdb03a85977c9811444f0dccc24f
vincenttang1101 <tangtrinhquang@gmail.com>
2024-01-20 09:31:08 +0700

    Refactor: addon @src, fix import conventions #1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { Provider } from 'react-redux'
-import { store } from '@redux/store.ts'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from '@redux/store.ts'
+import App from '@src/App.tsx'
 import '@assets/styles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
